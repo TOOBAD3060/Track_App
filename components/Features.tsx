25531666d6e6780671694fe8188c126e35036591
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2023-12-05 01:01:50 +0100

    removed unnecessary borders

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,8 +4,7 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <section className="border-2 border-red-500 
-    flex-col flexCenter overflow-hidden bg-feature-bg bg-center 
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center 
     bg-no-repeat py-24 ">
       <div className="max-container flex padding-container 
       relative w-full justify-end">
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="max-container padding-container 
     flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 
-    xl:flex-row  border-2 border-red-500">
+    xl:flex-row">
 
         <div className="hero-map" />
 
@@ -64,7 +64,7 @@ const Hero = () => {
 
         </div>
 
-        <div className="relative flex flex-1 items-start border-2 border-blue-500">
+        <div className="relative flex flex-1 items-start ">
             <div className="relative z-20 flex w-[268px] flex-col gap-8 
             rounded-3xl bg-green-90 px-7 py-8 ">
                 <div className="flex flex-col">
